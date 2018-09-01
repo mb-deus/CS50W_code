7826188eecd1c1ff076da3ca316c08a7e82f64a0
Mark B <mark.benson@hotmail.co.uk>
2018-09-01 17:09:26 +1000

    sat010918

---
+++ project1/import.py
@@ -0,0 +1,27 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    print(f"Opening books.....")
+    # f = open("books.csv")
+    with open('books.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        print(f"Reading books.....")
+        # reader = csv.reader(f)
+        # next(reader, None)
+        print(f"Looping through books.....")
+        for ISBN, title, author, year in reader:
+            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn": ISBN, "title": title, "author": author, "year": year})        
+        print(f"Committing books.....")
+    db.commit()
+    print(f"Added all the books into the database.")
+
+if __name__ == "__main__":
+    main()
+
